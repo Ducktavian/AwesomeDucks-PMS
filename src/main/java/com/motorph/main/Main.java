4d3874a6faeb53b2961f176a0c49ed7e7b5262ac
ducktavian <jaxmedenilla@gmail.com>
2026-06-08 21:55:39 +0800

    initial commit

---
+++ src/main/java/com/motorph/main/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.motorph.main;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
