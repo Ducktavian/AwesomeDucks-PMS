72a2ded67b202f6bd3b7746974c8b1f15dbb6226
ducktavian <jaxmedenilla@gmail.com>
2026-06-29 00:23:12 +0800

    renamed ui.Login -> ui.login package; edited pom.xml

---
+++ src/test/java/com/motorph/service/AttendanceServiceTest.java
@@ -0,0 +1,39 @@
+package com.motorph.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AttendanceServiceTest {
+    @Test
+    void timeIn() {
+    }
+
+    @Test
+    void timeOut() {
+    }
+
+    @Test
+    void computeDailyHours() {
+    }
+
+    @Test
+    void computeTotalHours() {
+    }
+
+    @Test
+    void testComputeTotalHours() {
+    }
+
+    @Test
+    void getAllAttendance() {
+    }
+
+    @Test
+    void testGetAllAttendance() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
